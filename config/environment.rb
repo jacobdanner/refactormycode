0b3dc9cfcb9370d2045a159994a96091070a1383
David Naffis <naffis@MacPro.local>
2011-03-04 00:21:57 -0500

    more work to getting it running on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-
+RAILS_GEM_VERSION = '2.0.2'
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
   
