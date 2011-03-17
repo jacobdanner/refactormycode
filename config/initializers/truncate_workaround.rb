ee73486adbddcf1949227ec3d8add2f11b62808b
paul <paul@intridea.com>
2011-03-17 17:21:10 -0400

    Truncate workaround should NOT use length 1 for everything. D'oh.

--- config/initializers/truncate_workaround.rb
+++ config/initializers/truncate_workaround.rb
@@ -4,7 +4,7 @@ module ActionView
       def truncate(text, length=30, truncate_string="...")
         return if text.nil?
         l = length - truncate_string.chars.to_a.size
-        (text.chars.to_a.size > length ? text.chars.to_a[0...1].join + truncate_string : text).to_s
+        (text.chars.to_a.size > length ? text.chars.to_a[0...l].join + truncate_string : text).to_s
       end
     end
   end
