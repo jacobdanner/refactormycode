69924fe2c604ec2b8d51a63321810091af636773
paul <paul@intridea.com>
2011-03-24 11:12:10 -0400

    Cripple logger while allowing SOME logging to stdout - we need to update the rails version for this app

--- vendor/plugins/rails_log_stdout/init.rb
+++ vendor/plugins/rails_log_stdout/init.rb
@@ -18,9 +18,7 @@ when 2 then
   end
 
   # borrowed from Rails::Initializer#initialize_framework_logging
-  ([ :active_record, :action_controller, :action_mailer ] & Rails.configuration.frameworks).each do |framework|
+  ([ :active_record, :action_controller, :action_mailer ] ).each do |framework|
     framework.to_s.camelize.constantize.const_get("Base").logger = Rails.logger
   end
-  ActiveSupport::Dependencies.logger = Rails.logger
-  Rails.cache.logger = Rails.logger
 end
