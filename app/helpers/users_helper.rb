9fd1b9e264ae44862c730bc50612c1768bda09d4
paul <paul@intridea.com>
2011-03-24 16:17:06 -0400

    Inline gravatar urls

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,32 +1,32 @@
 module UsersHelper
   def avatar(user)
-    param = case user
-    when String
-      user
-    when User
-      user.email
-    end
+    param = user.respond_to?(:email) ? user.email : user
+    param ||= 'test@example.com'
+
     link_to_unless !user.is_a?(User), 
       image_tag(gravatar_url_for(param), :class => 'avatar', :size => '80x80'),
       (user_path(user) if user.is_a?(User))
   end
-  
+
   def friend_avatar(friend)
     link_to image_tag(gravatar_url_for(friend.email), :size => "20x20"), user_path(friend), :title => h(friend.name)
   end
-  
+
   def friend?(user)
     current_user.friends.include?(user)
   end
-  
-  def gravatar_url_for(email)
-    Gravatar.new(email).url
+
+  require 'digest/md5'
+  def gravatar_url_for(email, options = {})
+    addr = email.downcase
+    hash = Digest::MD5.hexdigest addr
+    "http://www.gravatar.com/avatar/#{hash}"
   end
-  
+
   def position(user)
     render :partial => 'users/position', :locals => { :user => user }
   end
-    
+
   def link_to_website(user)
     link_to user.website, linkify(user.website) unless user.website.blank?
   end
