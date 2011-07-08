6207efa95f495e524549976043c6ff882b388489
Tim Cooper <coop@latrobest.com>
2011-07-08 22:01:39 +1000

    Add some tests around Code model

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,3 +1,4 @@
 Factory.define :user do |u|
   u.name Faker::Name.name
+  u.email Faker::Internet.email
 end
--- spec/models/code_spec.rb
+++ spec/models/code_spec.rb
@@ -1,1 +1,17 @@
 require 'spec_helper'
+
+describe Code do
+  let(:code) {Factory :code}
+
+  it { should respond_to(:user_name) }
+
+  it "delegates user_name to associated user" do
+    code.user_name.should eq(code.user.name)
+  end
+
+  it "creates notification after_save" do
+    code = Code.new Factory.attributes_for(:code).merge(:notify_me => '1')
+    code.user = Factory :user
+    expect{code.save}.to change{Notification.count}.by(1)
+  end
+end
