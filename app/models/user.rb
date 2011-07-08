f1b36d0c29c26fad2fb1a690a7c7530437e5e73d
Tim Cooper <coop@latrobest.com>
2011-07-08 21:17:31 +1000

    Whitespace.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,32 +6,30 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, :through => :friendships, :class_name => "User"
   has_many :authentications
-  
+
   alias_attribute :nickname, :name
-  
-  # validates_presence_of :identity_url
-  
+
   attr_protected :admin, :rating, :refactors_count
-  
+
   before_create :generate_missing_name
-  
+
   alias_method :fans, :friends
-  
+
   def create_token!
     self.token = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{name}--")
     save(false)
     self.token
   end
-  
-  def position    
+
+  def position
     User.find(:all, :order      => 'rating desc, refactors_count desc',
                     :conditions => ['rating >= ?', rating.to_i]).index(self) + 1
   end
-  
+
   def rated?(refactor)
     ratings.any? { |rating| rating.refactor == refactor }
   end
-  
+
   def position_image
     image = if position < 10
       position
@@ -44,10 +42,10 @@ class User < ActiveRecord::Base
     else
       'top10000'
     end
-    
+
     "#{RAILS_ROOT}/public/images/positions/#{image}.gif"
   end
-  
+
   private
     def generate_missing_name
       self.name = self.authentications.map(&:uname).compact.first if name.blank?
