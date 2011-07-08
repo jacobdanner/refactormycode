79fa9d3508ebf6cc9c1d1d22e1826575d488a492
Tim Cooper <coop@latrobest.com>
2011-07-08 21:21:24 +1000

    Whitespace and remove comments (we have version control).

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,37 +1,34 @@
 class Code < ActiveRecord::Base
   has_many :refactors, :conditions => { :spam => false }, :dependent => :destroy
   belongs_to :user
-  
+
   has_permalink :title
   attr_accessor :notify_me
-  
+
   validates_presence_of :title, :comment, :code, :language, :user_id
   validate              :email_if_notify_me
-  
+
   acts_as_defensio_article :fields => { :author       => :user_name,
                                         :author_email => :user_email,
                                         :content      => :comment,
                                         :permalink    => :permalink_url }
 
-  # acts_as_ferret :fields => [:title, :comment, :code, :language, :tag_list],
-  #                :single_index => true
-  
   delegate :name, :to => :user, :prefix => true
 
   acts_as_taggable_on :tags
-  
+
   attr_protected :user_id, :user
-  
+
   after_save     :create_notification
-  
+
   def to_param
     "#{id}-#{permalink}"
   end
-  
+
   def user_email
     user.email || "no-email@refactormycode.com"
   end
-  
+
   def permalink_url
     if ENV['RAILS_ENV'] == 'production'
       "http://refactormycode.com/codes/#{to_param}"
@@ -39,7 +36,7 @@ class Code < ActiveRecord::Base
       "http://refactormycode-dev.com/codes/#{to_param}"
     end
   end
-  
+
   def self.popular_sql_order
     case connection.adapter_name
     when /MySQL/
@@ -50,16 +47,16 @@ class Code < ActiveRecord::Base
       '1'
     end
   end
-  
+
   def self.search(text, options={})
     find_with_ferret(text, { :models => :all }.merge(options))
   end
-  
+
   private
     def email_if_notify_me
       errors.add :user, 'email is required for notification' if (user.nil? || user.email.blank?) && notify_me == '1'
     end
-  
+
     def create_notification
       if notify_me == '1' && Notification.find_by_code_id_and_email(id, user.email).nil?
         Notification.create :code => self, :user => user,
